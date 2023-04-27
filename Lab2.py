7d9050359249d0a9085f863ce9b16b16133b8d36
Celine2.0 <celine0242@gmail.com>
2023-04-27 11:40:38 +0800

    BMI checker

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,17 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    BMI = weight/(height**2)
+    print("BMI = " + str(BMI) + "\n")
+    if (BMI<18.5):
+        print("Under weight")
+    elif (BMI>25):
+        print("over weight")
+    else:
+        print("Normal weight")
+
+#calculate_bmi(weight="57", height="1.73")
+
+calculate_bmi(1.73,57)
